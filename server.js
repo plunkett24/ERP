b403ad91944bbbd7971deb2fdae195685321a8b4
Jamie Plunkett <betsymaeplunkett@gmail.com>
2020-09-06 18:13:30 -0600

    views/index.ejs

--- server.js
+++ server.js
@@ -3,10 +3,29 @@ const app=express();
 
 const PORT= process.env.PORT || 4000;
 
-app.get("/", (req, res)=>{
-res.send("HELLO");
+app.set("view engine", "ejs");
 
+
+app.get("/", (req, res)=>{
+res.render ("index");
 });
+
+app.get("/users/register", (req, res) =>{
+res.render("register");
+});
+
+app.get("/users/login", (req, res) =>{
+    res.render("login");
+});
+
+app.get("/users/dashboard", (req, res) =>{
+    res.render("dashboard",{ user:"Jamie"});
+    })
+
+
+
+
+
 
 app.listen(PORT, ()=>{
 console.log('Server running on port ${PORT}');
