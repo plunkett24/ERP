6b846e9281db8f516c8998a66d92a371e83928b2
Jamie Plunkett <betsymaeplunkett@gmail.com>
2020-09-07 23:34:53 -0600

    committ all is good again

--- views/dbConfig.js
+++ views/dbConfig.js
@@ -1,13 +1,15 @@
 require("dotenv").config();
 
-const { Pool }= require("pg");
+const { Pool } = require("pg");
 
-const isProduction= process.env.NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === "production";
 
-const connectionString = 'postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}';
+const connectionString =
+	//	"postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}";
+	"postgresql://jamplunkett:password@localhost:5432/nodelogin";
 
-const pool= new Pool({
-connectionString: isProduction ? process.env.DATABASE_URL : connectionString
+const pool = new Pool({
+	connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
 });
 
-module.exports ={pool};
+module.exports = { pool };
