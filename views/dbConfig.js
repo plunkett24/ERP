b051e7047ca91e163f5896da3a653682ddd52e3a
jamie <jamie@penguin>
2021-03-18 09:35:28 -0400

    newest commit

--- views/dbConfig.js
+++ views/dbConfig.js
@@ -8,7 +8,7 @@ const isProduction = process.env.NODE_ENV === 'production'; //look to see if app
 
 const connectionString =
 	//'postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}'
-	'postgresql://jamplunkett:password@localhost:5432/nodelogin';
+	'postgresql://postgres:password@localhost:5432/nodelogin';
 //connecting postgresql database to my nodejs .env file
 const pool = new Pool({
 	connectionString: isProduction ? process.env.DATABASE_URL : connectionString, //inialize a new pool
